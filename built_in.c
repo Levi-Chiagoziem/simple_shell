17d2c3b0a1620cc255956bdebd00bcbfb9d1c46b
Emmanuel <sobowalegz2@gmail.com>
2023-04-25 12:16:12 -0700

    functiin that checks for built in commands

--- built_in.c
+++ built_in.c
@@ -1,15 +1,21 @@
 #include "main.h"
+#include <unistd.h>
 
+/**
+ * check_builtin - function that checks for built in command
+ * @args: parameter that stores the command to be checked
+ * Return: return O when successful and 1 otherwise
+*/
 int check_builtin(char **args)
 {
 	built_in ar[] = {
-		{"exit", exit_shell},
+	{"exit", exit_shell},
 		{"env", print_env},
 		{NULL, NULL}
 	};
 	int i;
-	extern char **environ;
-	
+	char **environ = __environ; /* define environ inside the function*/
+
 	for (i = 0; ar[i].exe; i++)
 	{
 		if (!strcmp(args[0], ar[i].exe))
@@ -20,3 +26,4 @@ int check_builtin(char **args)
 	}
 	return (0);
 }
+
