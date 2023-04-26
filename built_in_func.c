df7cc18b81f63ff5bd188e36bd3656467cadb714
Levi <Levichiagoziem@gmail.com>
2023-04-26 05:20:15 -0700

    all tasks

--- built_in_func.c
+++ built_in_func.c
@@ -1,171 +1,192 @@
-#include "main.h"
-/**
- * exit-shell - Exit shell. Status default zero
- * @args: arguments
- *
- * Return: 1
- */
-int exit_shell(char **args,
-		char __attribute__((unused))**env)
-{
-	int id = getpid();
-	int status = 0;
-
-	if (args[1])
-		status = atoi(args[1]);
-
-	if (id != 0)
-		exit(status);
-	return (1);
-}
+#include "shell.h"
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
 /**
- * print_env - Prints environment variables
- * @args: arguments
- * @env: enviroment variables
+ * get_builtin - Matches a command with a corresponding
+ *               shellby builtin function.
+ * @command: The command to match.
  *
- * Return: 1
+ * Return: A function pointer to the corresponding builtin.
  */
-int print_env(char __attribute__((unused))**args,
-		char __attribute__((unused))**env)
+int (*get_builtin(char *command))(char **args, char **front)
 {
-	static Node *head, *tmp;
-	head = getenv_list();
-	
-	tmp = head;
-	while(tmp->next)
-	{
-		print_node(tmp);
-		write(1, "\n", 1);
-		tmp = tmp->next;
-	}
-	print_node(tmp);
-	write(1, "\n", 1);
-	return (1);
-}
-
-void print_node(Node *ptr)
-{
-	write(1, ptr->name, strlen(ptr->name));
-	write(1, "=", 1);
-	write(1, ptr->value, strlen(ptr->value));
-}
-
-int set_env(char **args, char __attribute__((unused))**environ)
-{
-	Node *head = getenv_list(), *tmp, *tmp1;
-
-	if (!args[1])
-	{
-		perror("No VARIABLE passed");
-		return (-1);
-	}
-	if (!args[2])
-	{
-		perror("No VALUE passed");
-		return (-1);
-	}
-	if (args[1] && args[2])
-	{
-		tmp = head;
-		while (tmp->next)
-		{
-			if (strcmp(tmp->name, args[1]) == 0)
-			{
-				tmp->value = strdup(args[2]);
-				break;
-			}
-			tmp = tmp->next;
-		}
-		if (!tmp->next)
-		{
-			tmp1 = malloc(sizeof(Node));
-			tmp1->name = strdup(args[1]);
-			tmp1->value = strdup(args[2]);
-			tmp1->next = NULL;
-			tmp->next = tmp1;
-		}
-	}
-        return (1);	
-}
-
-Node *getenv_list(void)
-{
-	extern char **environ;
-	char *env, *name, *value;
-        static Node *head;
-        Node *tmp = NULL, *tmp1 = NULL;
-	int i, j, len;
-
-	if (!head)
-	{
-		for(i = 0; environ[i]; i++)
-		{
-			len = 0;
-			env = environ[i];
-			for (j = 0; env[j] != '='; j++)
-			{
-				len++;
-			}
-			name = malloc(sizeof(char) * (len + 1));
-			value = env + j + 1;
-			strncat(name, env, len);
-			tmp = malloc(sizeof(Node));
-			tmp->name = strdup(name);
-			tmp->value = strdup(value);
-			tmp->next = NULL;
-			if (!head)
-				head = tmp;
-			else
-			{
-				tmp1 = head;
-				while(tmp1->next)
-				{
-					tmp1 = tmp1->next;
-				}
-				tmp1->next = tmp;
-			}
-		}
-	}
-        return (head);
-}
-int unset_env(char **args, char __attribute__((unused))**env)
-{
-	Node *head = getenv_list(), *next, *curr;
+	builtin_t funcs[] = {
+		{ "exit", shellby_exit },
+		{ "env", shellby_env },
+		{ "setenv", shellby_setenv },
+		{ "unsetenv", shellby_unsetenv },
+		{ "cd", shellby_cd },
+		{ "alias", shellby_alias },
+		{ "help", shellby_help },
+		{ NULL, NULL }
+	};
 	int i;
 
-	curr = head;
-	for (i = 0; args[i]; i++)
-		i++;
-	if (i > 2)
+	for (i = 0; funcs[i].name; i++)
 	{
-		perror("Too many Arguments");
-		return (-1);
-	}
-	while (curr->next)
-	{
-		next = curr->next;
-		if (strcmp(next->name, args[1]) == 0)
-		{
-			curr->next = NULL;
-			curr->next = next->next;
-			free_node(next);
+		if (_strcmp(funcs[i].name, command) == 0)
 			break;
-		}
-		curr = curr->next;
 	}
-	if (!curr->next)
-	{
-		perror("Env not found");
-		return (-1);
-	}
-	return (1);
+	return (funcs[i].f);
 }
 
-
-void free_node(Node *ptr)
+/**
+ * shellby_exit - Causes normal process termination
+ *                for the shellby shell.
+ * @args: An array of arguments containing the exit value.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If there are no arguments - -3.
+ *         If the given exit value is invalid - 2.
+ *         O/w - exits with the given status value.
+ *
+ * Description: Upon returning -3, the program exits back in the main function.
+ */
+int shellby_exit(char **args, char **front)
 {
-	free(ptr->name);
-	free(ptr->value);
-	free(ptr);
+	int i, len_of_int = 10;
+	unsigned int num = 0, max = 1 << (sizeof(int) * 8 - 1);
+
+	if (args[0])
+	{
+		if (args[0][0] == '+')
+		{
+			i = 1;
+			len_of_int++;
+		}
+		for (; args[0][i]; i++)
+		{
+			if (i <= len_of_int && args[0][i] >= '0' && args[0][i] <= '9')
+				num = (num * 10) + (args[0][i] - '0');
+			else
+				return (create_error(--args, 2));
+		}
+	}
+	else
+	{
+		return (-3);
+	}
+	if (num > max - 1)
+		return (create_error(--args, 2));
+	args -= 1;
+	free_args(args, front);
+	free_env();
+	free_alias_list(aliases);
+	exit(num);
+}
+
+/**
+ * shellby_cd - Changes the current directory of the shellby process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If the given string is not a directory - 2.
+ *         If an error occurs - -1.
+ *         Otherwise - 0.
+ */
+int shellby_cd(char **args, char __attribute__((__unused__)) **front)
+{
+	char **dir_info, *new_line = "\n";
+	char *oldpwd = NULL, *pwd = NULL;
+	struct stat dir;
+
+	oldpwd = getcwd(oldpwd, 0);
+	if (!oldpwd)
+		return (-1);
+
+	if (args[0])
+	{
+		if (*(args[0]) == '-' || _strcmp(args[0], "--") == 0)
+		{
+			if ((args[0][1] == '-' && args[0][2] == '\0') ||
+					args[0][1] == '\0')
+			{
+				if (_getenv("OLDPWD") != NULL)
+					(chdir(*_getenv("OLDPWD") + 7));
+			}
+			else
+			{
+				free(oldpwd);
+				return (create_error(args, 2));
+			}
+		}
+		else
+		{
+			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode)
+					&& ((dir.st_mode & S_IXUSR) != 0))
+				chdir(args[0]);
+			else
+			{
+				free(oldpwd);
+				return (create_error(args, 2));
+			}
+		}
+	}
+	else
+	{
+		if (_getenv("HOME") != NULL)
+			chdir(*(_getenv("HOME")) + 5);
+	}
+
+	pwd = getcwd(pwd, 0);
+	if (!pwd)
+		return (-1);
+
+	dir_info = malloc(sizeof(char *) * 2);
+	if (!dir_info)
+		return (-1);
+
+	dir_info[0] = "OLDPWD";
+	dir_info[1] = oldpwd;
+	if (shellby_setenv(dir_info, dir_info) == -1)
+		return (-1);
+
+	dir_info[0] = "PWD";
+	dir_info[1] = pwd;
+	if (shellby_setenv(dir_info, dir_info) == -1)
+		return (-1);
+	if (args[0] && args[0][0] == '-' && args[0][1] != '-')
+	{
+		write(STDOUT_FILENO, pwd, _strlen(pwd));
+		write(STDOUT_FILENO, new_line, 1);
+	}
+	free(oldpwd);
+	free(pwd);
+	free(dir_info);
+	return (0);
+}
+
+/**
+ * shellby_help - Displays information about shellby builtin commands.
+ * @args: An array of arguments.
+ * @front: A pointer to the beginning of args.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - 0.
+ */
+int shellby_help(char **args, char __attribute__((__unused__)) **front)
+{
+	if (!args[0])
+		help_all();
+	else if (_strcmp(args[0], "alias") == 0)
+		help_alias();
+	else if (_strcmp(args[0], "cd") == 0)
+		help_cd();
+	else if (_strcmp(args[0], "exit") == 0)
+		help_exit();
+	else if (_strcmp(args[0], "env") == 0)
+		help_env();
+	else if (_strcmp(args[0], "setenv") == 0)
+		help_setenv();
+	else if (_strcmp(args[0], "unsetenv") == 0)
+		help_unsetenv();
+	else if (_strcmp(args[0], "help") == 0)
+		help_help();
+	else
+		write(STDERR_FILENO, name, _strlen(name));
+
+	return (0);
 }
--- split.c
+++ split.c
@@ -1,46 +1,104 @@
-#include "main.h"
+#include "shell.h"
+
+int token_len(char *str, char *delim);
+int count_tokens(char *str, char *delim);
+char **_strtok(char *line, char *delim);
 
 /**
- * split_string - function that splits the strings into tokens
- * @string: paramter that stores the string
- * @delimiter: parameter that stores the delimiter
- * Return: returns NULL if unsuccessful and  pointer to a char if successful
+ * token_len - Locates the delimiter index marking the end
+ *             of the first token contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
  *
-*/
-
-char **split_string(char *string, char *delimiter)
+ * Return: The delimiter index marking the end of
+ *         the intitial token pointed to be str.
+ */
+int token_len(char *str, char *delim)
 {
-	char *token, **token_arrray, str_copy;
-	int i;
-	int token_num;
+	int index = 0, len = 0;
 
-	token_num = 0;
-	i = 0;
-	str_copy = strdup(string);
-	if (str_copy == NULL)
+	while (*(str + index) && *(str + index) != *delim)
 	{
-		perror("memory allocation error");
+		len++;
+		index++;
+	}
+
+	return (len);
+}
+
+/**
+ * count_tokens - Counts the number of delimited
+ *                words contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The number of words contained within str.
+ */
+int count_tokens(char *str, char *delim)
+{
+	int index, tokens = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != *delim)
+		{
+			tokens++;
+			index += token_len(str + index, delim);
+		}
+	}
+
+	return (tokens);
+}
+
+/**
+ * _strtok - Tokenizes a string.
+ * @line: The string.
+ * @delim: The delimiter character to tokenize the string by.
+ *
+ * Return: A pointer to an array containing the tokenized words.
+ */
+char **_strtok(char *line, char *delim)
+{
+	char **ptr;
+	int index = 0, tokens, t, letters, l;
+
+	tokens = count_tokens(line, delim);
+	if (tokens == 0)
 		return (NULL);
-	}
-	token = strtok(str_copy, delimiter);
 
-	while (token != NULL)
+	ptr = malloc(sizeof(char *) * (tokens + 2));
+	if (!ptr)
+		return (NULL);
+
+	for (t = 0; t < tokens; t++)
 	{
-		token_num++;
-		token = strtok(NULL, delimiter);
-	}
-	token_array = malloc(sizeof(char) * (token_num + 1));
-	strcpy(str_copy, string);
+		while (line[index] == *delim)
+			index++;
 
-	token = strtok(str_copy, delimiter);
-	for (i = 0; token != NULL; i++)
-	{
-		token_array[i] = malloc(sizeof(char) * strlen(token) + 2);
-		strcpy(token_array[i], token);
-		strcat(tokne_array[i], "/");
+		letters = token_len(line + index, delim);
 
-		token = strtok(NULL, delimiter);
+		ptr[t] = malloc(sizeof(char) * (letters + 1));
+		if (!ptr[t])
+		{
+			for (index -= 1; index >= 0; index--)
+				free(ptr[index]);
+			free(ptr);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+		{
+			ptr[t][l] = line[index];
+			index++;
+		}
+
+		ptr[t][l] = '\0';
 	}
-	token_array[i] = NULL;
-	return (token_array);
+	ptr[t] = NULL;
+	ptr[t + 1] = NULL;
+
+	return (ptr);
 }
